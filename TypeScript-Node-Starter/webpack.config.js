3024a369829a6528c7110cb5063ddf8f8c9a44dd
skomius <skomantas.juskys@outlook.com>
2020-06-08 08:05:19 +0300

    watch

--- TypeScript-Node-Starter/webpack.config.js
+++ TypeScript-Node-Starter/webpack.config.js
@@ -11,8 +11,6 @@ module.exports = {
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
-    watch: true,
-
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx"]
