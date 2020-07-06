159611cb259ecacb1b16ae6a2e14b8d19ce48da7
skomius <skomantas.juskys@outlook.com>
2020-07-06 05:04:51 -0700

    Update Tabs.tsx

--- TypeScript-Node-Starter/src/client/components/Tabs/Tabs.tsx
+++ TypeScript-Node-Starter/src/client/components/Tabs/Tabs.tsx
@@ -1,14 +1,14 @@
 import * as React from "react"
 
 export interface ITab {
-    label: string 
+    label: string;
 }
 
-export class ITabs {children: any[] }
+export class ITabs { children: any[] }
 
-export class Tabs extends React.Component<ITabs, {activeTabTitle: string}>{
+export class Tabs extends React.Component<ITabs, { activeTabTitle: string }>{
 
-    constructor(props: ITabs){
+    constructor(props: ITabs) {
         super(props)
         this.state = {
             activeTabTitle: this.props.children[0].props.label
@@ -16,33 +16,33 @@ export class Tabs extends React.Component<ITabs, {activeTabTitle: string}>{
 
         this.handleClick = this.handleClick.bind(this)
     }
-    
-    handleClick(event: any){
+
+    handleClick(event: any) {
         this.setState({
             activeTabTitle: event.target.innerText
         });
     }
 
-    render(){
+    render() {
 
         return (
-            <div className = "tab-list">
+            <div className="tab-list">
                 <ol>
                     {this.props.children.map((child: any) => {
-                        var classname = "tab-list-item"
-                        if(child.props.label === this.state.activeTabTitle)
+                        let classname = "tab-list-item"
+                        if (child.props.label === this.state.activeTabTitle)
                             classname = classname + " tab-list-active"
                         return (
-                        <li key={child.props.label} className = {classname} onClick={this.handleClick} >{child.props.label}</li>
+                            <li key={child.props.label} className={classname} onClick={this.handleClick} >{child.props.label}</li>
                         )
-                    } )}
+                    })}
                 </ol>
                 <div>
                     {this.props.children.map((child: any) => {
-                       if(child.props.label === this.state.activeTabTitle)
+                        if (child.props.label === this.state.activeTabTitle)
                             return child;
                         return undefined;
-                    } )}
+                    })}
                 </div>
             </div>
         )
